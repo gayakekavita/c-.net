92c8b47110901195451f04b5c1e79106209500d6
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-26 13:54:20 +0530

    Create arithmetic_operators.cs

---
+++ arithmetic_operators.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class first
+    {
+        static void Main(String[] args)
+        {
+
+
+            int a = 20;
+            int b = 10;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+    
+        }
+    }
+}
