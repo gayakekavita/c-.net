35c539f3910d5084dcc0a8f4f12ba39db5a94078
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-26 14:06:56 +0530

    Create mathFunctions.cs

---
+++ mathFunctions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class first
+    {
+        static void Main(String[] args)
+        {
+
+
+            /*math functions */
+
+            int a = Math.Max(34, 20);
+            Console.WriteLine(a);
+            int b = Math.Min(34, 54);
+            Console.WriteLine(b);
+            double c = Math.Sqrt(2);
+            Console.WriteLine(c);
+            int d = Math.Abs(-32);
+            Console.WriteLine(d);
+
+
+
+            Console.ReadLine();
+
+    
+        }
+    }
+}
